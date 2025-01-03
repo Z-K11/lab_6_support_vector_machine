1086b7895d1ce05959285a2e9e95d8d842b1c58b
Z-K11 <zulqarnainbazidkhail@gmail.com>
2025-01-03 22:15:58 +0500

    Compute Confusion Matrix & Write the function to display it

--- svm.py
+++ svm.py
@@ -24,3 +24,31 @@ print(y[0:5])
 x_train,x_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=4)
 print('Train Set',x_train.shape,y_train.shape)
 print('Test Set',x_test.shape,y_test.shape)
+from sklearn import svm
+model = svm.SVC(kernel='rbf')
+model.fit(x_train,y_train)
+predictions = model.predict(x_test)
+print(predictions[0:5])
+from sklearn.metrics import classification_report,confusion_matrix
+import itertools
+def plot_confusion_matrix(cm,classes,normalize=False,title='Confusion Matrix',cmap=plt.cm.Blues):
+    if normalize:
+        cm=cm.astype('float')/cm.sum(axis=1)[:,np.newaxis]
+        print("Normalized Confusion Matrix")
+    else:
+        print("Confusion Matrix")
+    print(cm)
+    plt.imshow(cm,interpolation='nearest',cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks=np.arange(len(classes))
+    plt.xticks(tick_marks,classes,rotation=45)
+    plt.yticks(tick_marks,classes)
+    fmt='.2f' if normalize else 'd'
+    thresh = cm.max()/2
+    for i,j in itertools.product(range(cm.shape[0]),range(cm.shape[1])):
+        plt.text(j,i,format(cm[i,j],fmt),horizontalalignment="center",color="white" if cm[i,j]>thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    cnf_matrix =confusion_matrix(y_test,predictions,labels=[2,4])
