2c26cf127d69b0f80c9057547af8eeb666d7b0a2
Z-K11 <zulqarnainbazidkhail@gmail.com>
2025-01-03 20:34:41 +0500

    Convert non numeric data types to integer

--- svm.py
+++ svm.py
@@ -8,4 +8,10 @@ print(cancer_data_df.head())
 # read the data from the csv file and save it into a pandas data frame
 ax =cancer_data_df[cancer_data_df['Class']==4][0:50].plot(kind="scatter",x='Clump',y="UnifSize",color='Red',label='malignant')
 cancer_data_df[cancer_data_df['Class']==2][0:50].plot(kind="scatter",x='Clump',y='UnifSize',color="Green",label='benign',ax=ax)
-plt.savefig("scatter.png")
+#Don't need to print the plot everytime commenting it out 
+#plt.savefig("scatter.png")
+print(cancer_data_df.dtypes)
+#prints the name of all the columns and their data types 
+cancer_data_df=cancer_data_df[pd.to_numeric(cancer_data_df['BareNuc'],errors='coerce').notnull()]
+cancer_data_df['BareNuc']=cancer_data_df['BareNuc'].astype('int')
+print(cancer_data_df.dtypes)
