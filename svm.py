ad93960703451a26e444da7cb30e966d07abf720
Z-K11 <zulqarnainbazidkhail@gmail.com>
2025-01-03 20:20:17 +0500

    Create a scatter plot for visual representation of the dataset

--- svm.py
+++ svm.py
@@ -6,3 +6,6 @@ import matplotlib.pyplot as plt
 cancer_data_df = pd.read_csv("cell_samples.csv")
 print(cancer_data_df.head())
 # read the data from the csv file and save it into a pandas data frame
+ax =cancer_data_df[cancer_data_df['Class']==4][0:50].plot(kind="scatter",x='Clump',y="UnifSize",color='Red',label='malignant')
+cancer_data_df[cancer_data_df['Class']==2][0:50].plot(kind="scatter",x='Clump',y='UnifSize',color="Green",label='benign',ax=ax)
+plt.savefig("scatter.png")
